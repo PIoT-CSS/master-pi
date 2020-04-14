bc2cb4f46c7d9c55660fbe3011bb53aa673f2ea5
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-04-14 23:28:16 +1000

    add login page and error pages

--- src/MasterCSS/templates/controllers.py
+++ src/MasterCSS/templates/controllers.py
@@ -1,8 +1,22 @@
-from flask import render_template, Blueprint
+from flask import (
+    render_template,
+    Blueprint,
+    request
+)
 
 # notify flask about external controllers
 controllers = Blueprint("template_controllers", __name__)
 
 @controllers.route("/")
-def login():
+def index():
     return render_template('index.html')
+
+@controllers.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # TODO: to implement login method
+        return render_template('todo.html')
