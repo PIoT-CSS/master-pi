511117cb1096ba164ee2c7a863a316490301bd4a
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-04-14 23:42:59 +1000

    defaulted routing for custom 404 and 401 pages

--- src/MasterCSS/controllers/templates.py
+++ src/MasterCSS/controllers/templates.py
@@ -7,10 +7,24 @@ from flask import (
 # notify flask about external controllers
 controllers = Blueprint("template_controllers", __name__)
 
+
 @controllers.route("/")
 def index():
     return render_template('index.html')
 
+
 @controllers.route("/login")
 def login():
     return render_template('login.html')
+
+
+# custom 404 page
+@controllers.errorhandler(404)
+def not_found(e):
+    return render_template("errors/404.html"), 404
+
+
+# custom 401/unauthorised page
+@controllers.errorhandler(401)
+def unauthorised(e):
+    return render_template("errors/401.html"), 401
