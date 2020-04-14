6e92d881445b58fd7232a447fc7405584fcc94e1
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-04-15 00:58:50 +1000

    added init_db function and added module docstrings to db.py

--- src/MasterCSS/db.py
+++ src/MasterCSS/db.py
@@ -1,13 +1,27 @@
+"""
+db.py module contains database setup.
+Currently using MySQL, awaiting reply from teaching team if NoSQL is allowed.
+"""
 from flask import current_app, g
 import pymysql
+
+
+def init_db():
+    # obtain database cursor
+    cursor = g.db.cursor()
+    # initialise database if not exist
+    cursor.execute('create database if not exists ' +
+                   current_app.config['DATABASE'])
+    # select database for use
+    g.db.select_db(current_app.config['DATABASE'])
 
 
 def get_db():
     if 'db' not in g:
         g.db = pymysql.connect(host=current_app.config['MYSQL_HOST'],
                                user=current_app.config['MYSQL_USERNAME'],
-                               password=current_app.config['MYSQL_PASSWORD'],
-                               db=current_app.config['DATABASE'])
+                               password=current_app.config['MYSQL_PASSWORD'])
+        init_db()
         return g.db
 
 
