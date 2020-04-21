de0eaa874cf8157048d67b8e15c8d291ca75b289
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-04-21 22:04:52 +1000

    removed comments

--- src/MasterCSS/auth.py
+++ src/MasterCSS/auth.py
@@ -9,18 +9,15 @@ from MasterCSS.models.user import User
 import MasterCSS.db as Db
 
 def login(username, password):
-    # TODO else?
     user, err = User.login(username, password)
     if user:
         login_user(user)
     return err
 
 def register(first_name, last_name, username, email, password, phone_number, user_type):
-    # TODO input validation
     user, err = User.register(first_name, last_name, username, email, password, phone_number, user_type)
     if user:
         login_user(user)
-        # return result based on registration outcome
     return err
 
 def logout():
