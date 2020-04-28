f8bfab4c5e5310258f57fc91536a5b4c89f5a7d4
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-04-28 23:32:41 +1000

    added constructor to booking model

--- src/MasterCSS/models/booking.py
+++ src/MasterCSS/models/booking.py
@@ -20,12 +20,23 @@ class Booking(db.Model):
     __tablename__ = "Booking"
     ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     UserID = db.Column(db.Integer, nullable=False)
-    DateTimeBooked = db.Column(
-        db.DateTime, default=db.func.current_timestamp(), nullable=False)
+    DateTimeBooked = db.Column(db.DateTime, nullable=False)
     DateTimeStart = db.Column(db.DateTime, nullable=False)
     DateTimeEnd = db.Column(db.DateTime, nullable=False)
     Cost = db.Column(db.Float, nullable=False)
     PickupCoordinates = db.Column(db.Text, nullable=False)
     DropCoordinates = db.Column(db.Text, nullable=False)
-    Distance = db.Column(db.Text, nullable=False)
+    Distance = db.Column(db.Float, nullable=False)
     Status = db.Column(db.Text, nullable=False)
+
+    def __init__(self, UserID, DateTimeBooked, DateTimeStart, DateTimeEnd, Cost, PickupCoordinates, DropCoordinates, Distance, Status, ID=None):
+        self.ID = ID
+        self.UserID = UserID
+        self.DateTimeBooked = DateTimeBooked
+        self.DateTimeStart = DateTimeStart
+        self.DateTimeEnd = DateTimeEnd
+        self.Cost = Cost
+        self.PickupCoordinates = PickupCoordinates
+        self.DropCoordinates = DropCoordinates
+        self.Distance = Distance
+        self.Status = Status
