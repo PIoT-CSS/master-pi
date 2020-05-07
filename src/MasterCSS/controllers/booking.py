d4a90c9bd2ecb43cb2efbe95906209551b8e51ff
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-05-07 17:18:33 +1000

    added placeholder functions and html elements

--- src/MasterCSS/controllers/booking.py
+++ src/MasterCSS/controllers/booking.py
@@ -25,26 +25,56 @@ DEFAULT_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 controllers = Blueprint("booking_controllers", __name__)
 
+
 @login_required
-@controllers.route(BOOKING_API_URL+ '/cars/<int:car_id>', methods=['POST'])
+@controllers.route(BOOKING_API_URL + '/cars/<int:car_id>', methods=['POST'])
 def check_car_availability(car_id):
     # TODO: validate datetime input and return error
     # return_datetime cant be earlier than pickup_datetime
-    pickup_datetime = datetime.strptime(request.form.get('pickup_datetime'), HTML_DATETIME_FORMAT)
-    return_datetime = datetime.strptime(request.form.get('return_datetime'), HTML_DATETIME_FORMAT)
-    car=db.session.query(Car).filter_by(ID=car_id).scalar()
+    pickup_datetime = datetime.strptime(
+        request.form.get('pickup_datetime'), HTML_DATETIME_FORMAT)
+    return_datetime = datetime.strptime(
+        request.form.get('return_datetime'), HTML_DATETIME_FORMAT)
+    pickup_coordinates = request.form.get('pickup_coordinates')
+    return_coordinates = request.form.get('return_coordinates')
+    car = db.session.query(Car).filter_by(ID=car_id).scalar()
     # check booking status with Booking.Status and see what's active
     # if active, check datetime.
     # if len(car.Booking)
     # for booking in car.Bookings:
     #     pass
+
+    # CALCULATE PRICE and show
+    # show pickup and drop locations
+    # cost =
     available = True
-    return render_template('booking/availability.html', car=car, pickup_datetime=pickup_datetime, return_datetime=return_datetime, available=available)
+    return render_template(
+        'booking/availability.html',
+        car=car,
+        pickup_datetime=pickup_datetime,
+        return_datetime=return_datetime,
+        pickup_coordinates=pickup_coordinates,
+        return_coordinates=return_coordinates,
+        # cost = cost,
+        available=available
+    )
+
 
 @login_required
-@controllers.route(BOOKING_API_URL+ '/book', methods=['POST'])
+@controllers.route(BOOKING_API_URL + '/book', methods=['POST'])
 def book():
-    pickup_datetime = datetime.strptime(request.form.get('pickup_datetime'), DEFAULT_DATETIME_FORMAT)
-    return_datetime = datetime.strptime(request.form.get('return_datetime'), DEFAULT_DATETIME_FORMAT)
-    car=db.session.query(Car).filter_by(ID=int(request.form.get('car_id'))).scalar()
-    return render_template('booking/success.html', car=car, pickup_datetime=pickup_datetime, return_datetime=return_datetime)
+    pickup_datetime = datetime.strptime(request.form.get(
+        'pickup_datetime'), DEFAULT_DATETIME_FORMAT)
+    return_datetime = datetime.strptime(request.form.get(
+        'return_datetime'), DEFAULT_DATETIME_FORMAT)
+    pickup_coordinates = request.form.get('pickup_coordinates')
+    return_coordinates = request.form.get('return_coordinates')
+    car = db.session.query(Car).filter_by(
+        ID=int(request.form.get('car_id'))).scalar()
+    # TODO booking takes place here
+    # pass booking details into page
+    return render_template(
+        'booking/success.html',
+        # booking=booking,
+        car=car
+    )
--- src/MasterCSS/controllers/templates.py
+++ src/MasterCSS/controllers/templates.py
@@ -68,7 +68,7 @@ def car_booking(car_id):
     car = db.session.query(Car).filter_by(ID=car_id).scalar()
     # TODO if car == None?
     # TODO show when the car is available?
-    return render_template('booking/car.html', car=car)
+    return render_template('booking/car.html', car=car, all_coordinates=car_coordinates)
 
 
 # custom 404 page
