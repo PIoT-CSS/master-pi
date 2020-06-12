4353d13023380693ef7026eb926550be766a578f
Austin <rockmanvnx6@gmail.com>
2020-06-12 15:06:51 +1000

    update Fri 12 Jun 2020 11:04:28 AEST

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -124,7 +124,6 @@ class SampleAssistant(object):
                 logging.info('End of audio request detected.')
                 logging.info('Stopping recording.')
                 self.conversation_stream.stop_recording()
-            print(resp.speech_results)
             if resp.speech_results:
                 logging.info('Transcript of user request: "%s".',
                              ' '.join(r.transcript
@@ -203,109 +202,103 @@ class SampleAssistant(object):
 
 
 
-@click.command()
-@click.option('--api-endpoint', default=ASSISTANT_API_ENDPOINT,
-              metavar='<api endpoint>', show_default=True,
-              help='Address of Google Assistant API service.')
-@click.option('--credentials',
-              metavar='<credentials>', show_default=True,
-              default=os.path.join(click.get_app_dir('google-oauthlib-tool'),
-                                   'credentials.json'),
-              help='Path to read OAuth2 credentials.')
-@click.option('--project-id',
-              metavar='<project id>',
-              default='a2-css-iot',
-              help=('Google Developer Project ID used for registration '
-                    'if --device-id is not specified'))
-@click.option('--device-model-id',
-              metavar='<device model id>',
-              default='a2-css-iot-car-share-smart-assistant-7jaln2',
-              help=(('Unique device model identifier, '
-                     'if not specifed, it is read from --device-config')))
-@click.option('--device-id',
-              metavar='<device id>',
-              default='e469fd48-a4ad-11ea-8b53-b827ebae6e93',
-              help=(('Unique registered device instance identifier, '
-                     'if not specified, it is read from --device-config, '
-                     'if no device_config found: a new device is registered '
-                     'using a unique id and a new device config is saved')))
-@click.option('--device-config', show_default=True,
-              metavar='<device config>',
-              default=os.path.join(
-                  click.get_app_dir('googlesamples-assistant'),
-                  'device_config.json'),
-              help='Path to save and restore the device configuration')
-@click.option('--lang', show_default=True,
-              metavar='<language code>',
-              default='en-US',
-              help='Language code of the Assistant')
-@click.option('--display', is_flag=True, default=False,
-              help='Enable visual display of Assistant responses in HTML.')
-@click.option('--verbose', '-v', is_flag=True, default=False,
-              help='Verbose logging.')
-@click.option('--input-audio-file', '-i',
-              metavar='<input file>',
-              help='Path to input audio file. '
-              'If missing, uses audio capture')
-@click.option('--output-audio-file', '-o',
-              metavar='<output file>',
-              help='Path to output audio file. '
-              'If missing, uses audio playback')
-@click.option('--audio-sample-rate',
-              default=audio_helpers.DEFAULT_AUDIO_SAMPLE_RATE,
-              metavar='<audio sample rate>', show_default=True,
-              help='Audio sample rate in hertz.')
-@click.option('--audio-sample-width',
-              default=audio_helpers.DEFAULT_AUDIO_SAMPLE_WIDTH,
-              metavar='<audio sample width>', show_default=True,
-              help='Audio sample width in bytes.')
-@click.option('--audio-iter-size',
-              default=audio_helpers.DEFAULT_AUDIO_ITER_SIZE,
-              metavar='<audio iter size>', show_default=True,
-              help='Size of each read during audio stream iteration in bytes.')
-@click.option('--audio-block-size',
-              default=audio_helpers.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE,
-              metavar='<audio block size>', show_default=True,
-              help=('Block size in bytes for each audio device '
-                    'read and write operation.'))
-@click.option('--audio-flush-size',
-              default=audio_helpers.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE,
-              metavar='<audio flush size>', show_default=True,
-              help=('Size of silence data in bytes written '
-                    'during flush operation'))
-@click.option('--grpc-deadline', default=DEFAULT_GRPC_DEADLINE,
-              metavar='<grpc deadline>', show_default=True,
-              help='gRPC deadline in seconds')
-@click.option('--once', default=False, is_flag=True,
-            help='Force termination after a single conversation.')
+# @click.command()
+# @click.option('--api-endpoint', default=ASSISTANT_API_ENDPOINT,
+#               metavar='<api endpoint>', show_default=True,
+#               help='Address of Google Assistant API service.')
+# @click.option('--credentials',
+#               metavar='<credentials>', show_default=True,
+#               default=os.path.join(click.get_app_dir('google-oauthlib-tool'),
+#                                    'credentials.json'),
+#               help='Path to read OAuth2 credentials.')
+# @click.option('--project-id',
+#               metavar='<project id>',
+#               default='a2-css-iot',
+#               help=('Google Developer Project ID used for registration '
+#                     'if --device-id is not specified'))
+# @click.option('--device-model-id',
+#               metavar='<device model id>',
+#               default='a2-css-iot-car-share-smart-assistant-7jaln2',
+#               help=(('Unique device model identifier, '
+#                      'if not specifed, it is read from --device-config')))
+# @click.option('--device-id',
+#               metavar='<device id>',
+#               default='e469fd48-a4ad-11ea-8b53-b827ebae6e93',
+#               help=(('Unique registered device instance identifier, '
+#                      'if not specified, it is read from --device-config, '
+#                      'if no device_config found: a new device is registered '
+#                      'using a unique id and a new device config is saved')))
+# @click.option('--device-config', show_default=True,
+#               metavar='<device config>',
+#               default=os.path.join(
+#                   click.get_app_dir('googlesamples-assistant'),
+#                   'device_config.json'),
+#               help='Path to save and restore the device configuration')
+# @click.option('--lang', show_default=True,
+#               metavar='<language code>',
+#               default='en-US',
+#               help='Language code of the Assistant')
+# @click.option('--display', is_flag=True, default=False,
+#               help='Enable visual display of Assistant responses in HTML.')
+# @click.option('--verbose', '-v', is_flag=True, default=False,
+#               help='Verbose logging.')
+# @click.option('--input-audio-file', '-i',
+#               metavar='<input file>',
+#               help='Path to input audio file. '
+#               'If missing, uses audio capture')
+# @click.option('--output-audio-file', '-o',
+#               metavar='<output file>',
+#               help='Path to output audio file. '
+#               'If missing, uses audio playback')
+# @click.option('--audio-sample-rate',
+#               default=audio_helpers.DEFAULT_AUDIO_SAMPLE_RATE,
+#               metavar='<audio sample rate>', show_default=True,
+#               help='Audio sample rate in hertz.')
+# @click.option('--audio-sample-width',
+#               default=audio_helpers.DEFAULT_AUDIO_SAMPLE_WIDTH,
+#               metavar='<audio sample width>', show_default=True,
+#               help='Audio sample width in bytes.')
+# @click.option('--audio-iter-size',
+#               default=audio_helpers.DEFAULT_AUDIO_ITER_SIZE,
+#               metavar='<audio iter size>', show_default=True,
+#               help='Size of each read during audio stream iteration in bytes.')
+# @click.option('--audio-block-size',
+#               default=audio_helpers.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE,
+#               metavar='<audio block size>', show_default=True,
+#               help=('Block size in bytes for each audio device '
+#                     'read and write operation.'))
+# @click.option('--audio-flush-size',
+#               default=audio_helpers.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE,
+#               metavar='<audio flush size>', show_default=True,
+#               help=('Size of silence data in bytes written '
+#                     'during flush operation'))
+# @click.option('--grpc-deadline', default=DEFAULT_GRPC_DEADLINE,
+#               metavar='<grpc deadline>', show_default=True,
+#               help='gRPC deadline in seconds')
+# @click.option('--once', default=False, is_flag=True,
+#             help='Force termination after a single conversation.')
 
-def main(api_endpoint, credentials, project_id,
-         device_model_id, device_id, device_config,
-         lang, display, verbose,
-         input_audio_file, output_audio_file,
-         audio_sample_rate, audio_sample_width,
-         audio_iter_size, audio_block_size, audio_flush_size,
-         grpc_deadline, once, *args, **kwargs):
+def main():
     # Variables
-    # api_endpoint = ASSISTANT_API_ENDPOINT
-    # credentials = os.path.join(click.get_app_dir('google-oauthlib-tool'),
-    #                                'credentials.json')
-    # project_id = "a2-css-iot"
-    # device_model_id = "a2-css-iot-car-share-smart-assistant-7jaln2"
-    # device_id = "e469fd48-a4ad-11ea-8b53-b827ebae6e93"
-    # device_config = os.path.join(
-    #               click.get_app_dir('googlesamples-assistant'),
-    #               'device_config.json')
-    # lang = "en-US"
-    # display=False
-    # verbose=False
-    # audio_sample_rate=audio_helpers.DEFAULT_AUDIO_SAMPLE_RATE
-    # audio_sample_width=audio_helpers.DEFAULT_AUDIO_SAMPLE_WIDTH
-    # audio_iter_size=audio_helpers.DEFAULT_AUDIO_ITER_SIZE
-    # audio_block_size=audio_helpers.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE
-    # audio_flush_size=audio_helpers.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE
-    # grpc_deadline=DEFAULT_GRPC_DEADLINE
-    # once=False
+    api_endpoint = ASSISTANT_API_ENDPOINT
+    credentials = os.path.join(click.get_app_dir('google-oauthlib-tool'),
+                                   'credentials.json')
+    project_id = "a2-css-iot"
+    device_model_id = "a2-css-iot-car-share-smart-assistant-7jaln2"
+    device_id = "e469fd48-a4ad-11ea-8b53-b827ebae6e93"
+    device_config = os.path.join(
+                  click.get_app_dir('googlesamples-assistant'),
+                  'device_config.json')
+    lang = "en-US"
+    display=False
+    verbose=False
+    audio_sample_rate=audio_helpers.DEFAULT_AUDIO_SAMPLE_RATE
+    audio_sample_width=audio_helpers.DEFAULT_AUDIO_SAMPLE_WIDTH
+    audio_iter_size=audio_helpers.DEFAULT_AUDIO_ITER_SIZE
+    audio_block_size=audio_helpers.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE
+    audio_flush_size=audio_helpers.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE
+    grpc_deadline=DEFAULT_GRPC_DEADLINE
+    once=False
     
     # Setup logging.
     logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
@@ -339,9 +332,7 @@ def main(api_endpoint, credentials, project_id,
             flush_size=audio_flush_size
         )
     )
-    
-    print("Source", audio_source)
-    
+        
     audio_sink = audio_device = (
         audio_device or audio_helpers.SoundDeviceStream(
             sample_rate=audio_sample_rate,
@@ -351,7 +342,6 @@ def main(api_endpoint, credentials, project_id,
         )
     )
     
-    print("Sink", audio_sink)
     # Create conversation stream with the given audio source and sink.
     conversation_stream = audio_helpers.ConversationStream(
         source=audio_source,
@@ -360,8 +350,6 @@ def main(api_endpoint, credentials, project_id,
         sample_width=audio_sample_width,
     )
     
-    print("ConversationStream", conversation_stream)
-
     if not device_id or not device_model_id:
         try:
             with open(device_config) as f:
